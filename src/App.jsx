723b671316d2f09e0d430f023037c747f7339cff
afrizalsofyan <afrizalafaandy@gmail.com>
2023-01-18 03:12:54 +0700

    feat: integration with backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,44 @@
+import { useEffect, useState } from 'react'
 import { Button, Card, Col, Container, Form, InputGroup, Row } from 'react-bootstrap'
 import { IoSearchOutline } from 'react-icons/io5'
 function App() {
+  const [feeds, setFeeds] = useState([])
+  const [feed, setFeed] = useState({})
+  const [errors, setErrors] = useState('')
+  const [page, setPage] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const fetchAPI = async () => {
+    const res = await fetch('http://localhost:3333/feeds')
+    const resBody = await res.json()
+    if (resBody?.success) {
+      setErrors('')
+      setFeeds(resBody?.data)
+      setFeed(resBody?.data[0])
+    } else {
+      setErrors('Error. There something wrong!!!')
+    }
+  }
+
+  const onPrevPage = () => {
+    if (page > 0) {
+      setPage(page - 1)
+      setFeed(feeds[page])
+      setCurrentPage(currentPage - 1)
+    }
+  }
+  const onNextPage = () => {
+    if (page <= feeds.length - 2) {
+      setPage(page + 1)
+      setFeed(feeds[page])
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
+  useEffect(() => {
+    fetchAPI()
+  }, [])
+
   return (
     <Container>
       <Row as={'header'} className='border-bottom border-primary'>
@@ -23,32 +61,38 @@ function App() {
       </Form>
       <Row>
         <Col className='d-flex justify-content-center'>
-          <Card className='w-75 p-3 d-flex justify-content-center flex-column shadow'>
-            <h4 className='text-center'>title</h4>
-            <span className='text-center'>@author</span>
-            <div className='w-100'>
-              <figure>
-                <img src='' alt='content-pic' />
-                <figcaption className='text-center'>blah</figcaption>
-              </figure>
-            </div>
-            <div className='d-flex justify-content-center'>
-              <Button>Show more</Button>
-            </div>
-          </Card>
+          {!errors ? (
+            <Card className='w-75 p-3 shadow'>
+              <div className='w-100'>
+                <div className='d-flex flex-column justify-content-center'>
+                  <div className='d-flex justify-content-center py-4'>
+                    <img src={feed?.media?.m} alt='content-pic' />
+                  </div>
+                  <div className='d-flex flex-column justify-content-center py-4'>
+                    {feed?.title ? <h4 className='text-center'>{feed?.title}</h4> : null}
+                    <span className='text-center'>{feed?.author?.split('"')[1]}</span>
+                  </div>
+                  <div className='d-flex flex-column justify-content-center py-4'>
+                    <span className='text-center'>Tag:</span>
+                    <p className='text-center'>{feed?.tags ? feed?.tags : '-'}</p>
+                  </div>
+                </div>
+              </div>
+              {/* <div dangerouslySetInnerHTML={{ __html: feed?.description }}></div> */}
+              <div className='d-flex justify-content-center'>
+                <Button>Show more</Button>
+              </div>
+            </Card>
+          ) : (
+            <span className='my-4 fs-2 fw-bold text-danger'>{errors}</span>
+          )}
         </Col>
-        {/* <Col className='bg-primary'>
-          a
-        </Col>
-        <Col className='bg-warning'>
-          a
-        </Col> */}
       </Row>
       <Row className='py-5'>
         <Col className='d-flex justify-content-center w-full gap-4 align-items-center'>
-          <Button>Prev</Button>
-          <span>1</span>
-          <Button>Next</Button>
+          <Button onClick={onPrevPage}>Prev</Button>
+          <span>{currentPage}</span>
+          <Button onClick={onNextPage}>Next</Button>
         </Col>
       </Row>
     </Container>
